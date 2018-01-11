a528d23c95ff7991cb953b80838cfedcfbe95198
Darrell Pawson <dpawson905@gmail.com>
2018-01-10 20:57:27 -0500

    Emit new message on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Darrell',
-    text: 'Test'
-  });
 });
 
 socket.on('disconnect', function () {
